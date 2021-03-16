50ee0aff545bd7a2dbf6e0dd53b6e5b9e3f8662c
Danjun Wang <mjwdj80@hotmail.com>
2021-03-16 12:01:23 +0800

    Update mattermost_install.sh

--- mattermost_install.sh
+++ mattermost_install.sh
@@ -4,7 +4,7 @@ DB_IP=192.168.10.31
 SERVER_IP=192.168.10.41
 DB_PASSWORD=!qaz2wsx
 DB_USER=mmuser
-DB_ROOT_PASSWORD=123456
+DB_ROOT_PASSWORD=Wdj@0562
 DB_NAME=mattermost
 
 dnf -y install mariadb
@@ -32,26 +32,4 @@ sed -i "/DriverName/s/postgres/mysql/" /opt/mattermost/config/config.json
 sed -i "/\"DataSource\"/s/\"postgres.*\"/\"$DB_USER:$DB_PASSWORD@tcp\($DB_IP:3306\)\/mattermost?charset=utf8mb4,utf8\\\\u0026readTimeout=30s\\\\u0026writeTimeout=30s\"/" /opt/mattermost/config/config.json
 firewall-cmd --add-port=8065/tcp --permanent
 firewall-cmd --reload
-#sudo -u mattermost /opt/mattermost/bin/mattermost &
-
-#configure the service
-echo '[Unit]
-Description=Mattermost
-After=syslog.target network.target
-
-[Service]
-Type=notify
-WorkingDirectory=/opt/mattermost
-User=mattermost
-ExecStart=/opt/mattermost/bin/mattermost
-PIDFile=/var/spool/mattermost/pid/master.pid
-TimeoutStartSec=3600
-LimitNOFILE=49152
-
-[Install]
-WantedBy=multi-user.target' > /etc/systemd/system/mattermost.service
-
-chmod 664 /etc/systemd/system/mattermost.service
-systemctl daemon-reload
-systemctl enable mattermost
-systemctl start mattermost
+sudo -u mattermost /opt/mattermost/bin/mattermost &
